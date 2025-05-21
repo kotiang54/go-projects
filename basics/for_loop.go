b3479c29f6788d292095ca2a0e40b1e8d7535c26
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-21 09:43:39 -0400

    update for_loop file

--- basics/for_loop.go
+++ basics/for_loop.go
@@ -3,7 +3,41 @@ package main
 import "fmt"
 
 func main() {
+
+	// Simple iteration over range
 	for i := 1; i < 5; i++ {
 		fmt.Println(i)
+	}
+
+	// Iterate over collection
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	for index, value := range numbers {
+		fmt.Printf("Index: %d, Value: %d\n", index, value)
+	}
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue // continue the loop but skip the rest of the line/statements
+		}
+		fmt.Println("Odd number: ", i)
+		if i == 5 {
+			break // break out of the loop
+		}
+	}
+
+	rows := 5
+
+	// Outer loop
+	for i := 1; i <= rows; i++ {
+		// inner llop for spaces
+		for j := 1; j <= rows-i; j++ {
+			fmt.Print(" ")
+		}
+
+		// inner loop for stars
+		for k := 1; k <= 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
 	}
 }
