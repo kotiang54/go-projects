d75691dbf8026af848bb9f9e21cf91a16e88ba56
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-22 13:37:22 -0400

    for_loop as a while loop in go

---
+++ basics/for_as_while.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for as while with break statement
+	sum := 0
+	for {
+		sum += 10
+		if sum > 50 {
+			break
+		}
+		fmt.Println("Sum ", sum)
+	}
+
+	// for as while with continue statement
+	num := 1
+	for num <= 10 {
+		if num&1 == 0 {
+			num++
+			continue
+		}
+		fmt.Println("Odd number: ", num)
+		num++
+	}
+}
