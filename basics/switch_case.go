08e875c024f6ee8752984d0b4fd2e4a3a1c45768
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-28 09:25:14 -0400

    first example of switch-case statement

--- basics/switch_case.go
+++ basics/switch_case.go
@@ -1,13 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Switch statement
-	switch expression {
-	case value1:
-		// Code to be executed if expression equals value1
-	case value2:
-		// Code to be executed if expresion equals value2
-	case value3:
-		// Code to be executed if expression equals value3
+	// Switch statement (switch, case, default) (fallthrough)
+	// switch expression {
+	// case value1:
+	// 	// Code to be executed if expression equals value1
+	// case value2:
+	// 	// Code to be executed if expresion equals value2
+	// case value3:
+	// 	// Code to be executed if expression equals value3
+	// default:
+	// 	// Code to be executed if expression does not match any value
+	// }
+
+	fruit := "banana"
+	switch fruit {
+	case "apple":
+		fmt.Println("It's an apple")
+	case "banana":
+		fmt.Println("It's a banana")
+	case "orange":
+		fmt.Println("It's an orange")
+	default:
+		fmt.Println("Unknown fruit")
 	}
 }
