3195a473a5302096349ede1f4ebcba00a7d34c70
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-28 15:56:40 -0400

    mor about arrays

--- basics/arrays.go
+++ basics/arrays.go
@@ -26,4 +26,28 @@ func main() {
 	for index, value := range fruits {
 		fmt.Printf("Index: %d, Value: %s\n", index, value)
 	}
+
+	// Compare arrays
+	array1 := [3]int{1, 2, 3}
+	array2 := [3]int{1, 2, 3}
+
+	fmt.Println("Array 1 is equal to array 2: ", array1 == array2)
+
+	// Multidimensional array e.g. matrix
+	var matrix [3][3]int = [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	fmt.Println(matrix)
+
+	arr := [3]int{10, 20, 30}
+	// var copied_arr *[3]int
+	copied_arr := &arr // copied_arr is a pointer variable and not an array variable in this case
+	copied_arr[0] = 15
+	fmt.Println(arr)
+	fmt.Println(copied_arr)
+
+	fmt.Println("Arr is equal copied_arr: ", arr == *copied_arr)
 }
