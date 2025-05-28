0e38199e742e5f63312f78c91ed9ecdafacfa98b
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-28 10:18:08 -0400

    more examples of switch-case statement

--- basics/switch_case.go
+++ basics/switch_case.go
@@ -16,6 +16,7 @@ func main() {
 	// }
 
 	fruit := "banana"
+
 	switch fruit {
 	case "apple":
 		fmt.Println("It's an apple")
@@ -25,5 +26,60 @@ func main() {
 		fmt.Println("It's an orange")
 	default:
 		fmt.Println("Unknown fruit")
+	}
+
+	// Multiple Conditions
+	day := "Monday"
+
+	switch day {
+	case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+		fmt.Println("It's a weekday")
+	case "Saturday", "Sunday":
+		fmt.Println("It's a weekend")
+	default:
+		fmt.Println("Invalid day")
+	}
+
+	number := 17
+
+	switch {
+	case number < 10:
+		fmt.Println("Number is less than 10.")
+	case number >= 10 && number < 20:
+		fmt.Println("Number is between 10 and 19.")
+	default:
+		fmt.Println("Number is 20 or more.")
+	}
+
+	// Fallthrough expression
+	num := 2
+
+	switch {
+	case num > 1:
+		fmt.Println("Greater than 1")
+		fallthrough
+	case num == 2:
+		fmt.Println("Number is 2")
+	default:
+		fmt.Println("Not a Two")
+	}
+
+	// Switch-case for type assertion
+	checkType(10)
+	checkType(3.14257)
+	checkType("Hello")
+	checkType(true)
+}
+
+func checkType(x interface{}) {
+	switch x.(type) {
+	case int:
+		fmt.Println("It's an integer")
+	case float64:
+		fmt.Println("It's a float")
+	case string:
+		fmt.Println("It's a string")
+	default:
+		fmt.Println("Unknown Type")
 	}
 }
