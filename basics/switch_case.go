333fbbec1b5e2f95e7dff7a6adf13a87c4f44467
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-28 09:15:02 -0400

    all about switch-case statements

---
+++ basics/switch_case.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// Switch statement
+	switch expression {
+	case value1:
+		// Code to be executed if expression equals value1
+	case value2:
+		// Code to be executed if expresion equals value2
+	case value3:
+		// Code to be executed if expression equals value3
+	}
+}
