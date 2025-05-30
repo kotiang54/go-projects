b6413e057d48a96f7353311ed524b4c5f824d219
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-30 12:00:48 -0400

    more statements on maps.go file

--- basics/maps.go
+++ basics/maps.go
@@ -1,6 +1,9 @@
-package main
+package basics
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
 
 func main() {
 	// var mapVariable map[keyType]valueType
@@ -44,14 +47,54 @@ func main() {
 
 	value, unknownValue := myMap["key1"]
 	fmt.Println("value: ", value)
-	fmt.Println("unknownValue: ", unknownValue)
+	fmt.Println("Is a value associated with key1?: ", unknownValue)
 
 	// the unknown component in the map call above is important
-	// like demonstrated in the if statement below:
+	// like demonstrated in the -if statement- below:
 
 	if _, ok := myMap["code"]; ok {
 		delete(myMap, "code")
 	}
 	fmt.Println(myMap)
 
+	myMap2 := map[string]int{"a": 1, "b": 2, "c": 3}
+	myMap3 := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	if maps.Equal(myMap2, myMap3) {
+		fmt.Println("myMap2 and myMap3 are equal!")
+	}
+
+	// iterating over a map
+	for key, value := range myMap2 {
+		fmt.Println(key, value)
+	}
+
+	var myMap4 map[string]string
+	if myMap4 == nil {
+		fmt.Println("The map is initialized to nil value.")
+	} else {
+		fmt.Println("The map is not initialized to nil value.")
+	}
+
+	val := myMap4["key"]
+	fmt.Println(val)
+
+	// if you initialize a map as above i.e., var myMap4 map[string]string
+	// you cannot populate the map using the map["key"] = value statement as shown below
+	// myMap4["key"] = "10"
+	// fmt.Println(myMap4)
+
+	// Correct way:
+	myMap4 = make(map[string]string)
+	myMap4["key"] = "10"
+	fmt.Println(myMap4)
+
+	fmt.Println("myMap4 length is", len(myMap4))
+
+	// nested maps
+	myMap5 := make(map[string]map[string]string)
+	fmt.Println(myMap5)
+
+	myMap5["map1"] = myMap4
+	fmt.Println(myMap5)
 }
