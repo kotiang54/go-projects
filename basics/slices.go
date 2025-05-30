71406958bd74387a5bf027d7d759e1141ce4112d
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-30 09:58:26 -0400

    more about slices

--- basics/slices.go
+++ basics/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	// Unlike arrays slices have no fixed length
@@ -16,10 +19,34 @@ func main() {
 	fmt.Println(nums2)
 
 	// Use make function to create a slice
-	slice := make([]int, 5)
-	slice = nums2[1:4]
-	fmt.Println(slice)
+	slice1 := make([]int, 5)
+	slice1 = nums2[1:4]
+	fmt.Println(slice1)
 
-	slice = append(slice, 6, 0)
-	fmt.Println(slice)
+	slice1 = append(slice1, 6, 0)
+	fmt.Println("Slice: ", slice1)
+
+	// make copy of a slice
+	slice_copy := make([]int, len(slice1))
+	copy(slice_copy, slice1)
+
+	fmt.Println("Copy slice: ", slice_copy)
+
+	for i, v := range slice_copy {
+		fmt.Println(i, ":", v)
+	}
+
+	// Access a slice
+	fmt.Println("Element at index 3 is", slice_copy[3])
+
+	// modify a slice value
+	// slice_copy[3] = 50
+	// fmt.Println("Element at index 3 after update is", slice_copy[3])
+
+	if slices.Equal(slice1, slice_copy) {
+		fmt.Println("slice1 is equal to slice_copy!")
+	} else {
+		fmt.Println("slice1 is not equal slice_copy!")
+	}
+
 }
