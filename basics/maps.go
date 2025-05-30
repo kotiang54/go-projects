d66ffd5bd22f2c78a0864ffc83a40134ec886b36
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-05-30 11:16:57 -0400

    more about maps

--- basics/maps.go
+++ basics/maps.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var mapvariable map[keyType]valueType
-	// mapvariable := make(map[keyType]valueType)
+	// var mapVariable map[keyType]valueType
+	// mapVariable := make(map[keyType]valueType)
+
 	// using a Map literal
-	// mapvariable := map[keyType]valueType{
+	// mapVariable := map[keyType]valueType{
 	// 	key1: value1,
 	// 	key2: value2,
 	// }
@@ -31,19 +32,26 @@ func main() {
 	delete(myMap, "key")
 	fmt.Println(myMap)
 
-	if _, ok := myMap["code"]; ok {
-		delete(myMap, "code")
-	}
-	fmt.Println(myMap)
-
-	// delete the entire mare
+	// delete the entire map
 	myMap["key1"] = 11
 	myMap["code1"] = 18
 	myMap["key2"] = 12
 	myMap["code2"] = 19
 	fmt.Println(myMap)
 
-	clear(myMap)
+	// clear(myMap)
+	fmt.Println(myMap)
+
+	value, unknownValue := myMap["key1"]
+	fmt.Println("value: ", value)
+	fmt.Println("unknownValue: ", unknownValue)
+
+	// the unknown component in the map call above is important
+	// like demonstrated in the if statement below:
+
+	if _, ok := myMap["code"]; ok {
+		delete(myMap, "code")
+	}
 	fmt.Println(myMap)
 
 }
