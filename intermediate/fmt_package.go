6834e7bb6a0dbb719ac06c83efaf06a9112cea3f
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-06-08 11:28:54 -0400

    add scanning functions

--- intermediate/fmt_package.go
+++ intermediate/fmt_package.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Print
+	/*/ fmt.Print
 	fmt.Print("Hello ")
 	fmt.Print("World! ")
 	fmt.Print("13, 1999")
@@ -21,8 +21,32 @@ func main() {
 	fmt.Printf("Name: %s, age: %d\n", name, age)
 	fmt.Printf("Binary: %b, Hex: %#X\n", age, age)
 
-	// Formating functions
+	// Formating Functions
+	// fmt.Sprint
 	fmt.Println("")
-	s := fmt.Sprint("Hello ", "World! ", 123, 456)
-	fmt.Println(s)
+	s := fmt.Sprint("Hello", "World!", 123, 456)
+	fmt.Print(s)
+
+	// fmt.Sprintln
+	s = fmt.Sprintln("Hello", "World!", 123, 456)
+	fmt.Print(s)
+	fmt.Print(s)
+
+	// fmt.Sprintf
+	sf := fmt.Sprintf("Name: %s, Age: %d", name, age)
+	fmt.Println(sf)
+
+	// Scanning Functions */
+	var name string
+	var age int
+
+	fmt.Print("Enter your name and age:")
+	fmt.Scan(&name, &age)
+
+	// Scanln stops scanning at each new line
+	// fmt.Scanln(&name, &age)
+
+	// fmt.Scanf("%s %d", &name, &age)
+	fmt.Printf("Name: %s, Age: %d\n", name, age)
+
 }
