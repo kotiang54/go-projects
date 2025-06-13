d99b6fd98dfc315a5eb114ebfb05a23f802edad6
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-06-13 09:07:15 -0400

    various types of methods

--- intermediate/methods.go
+++ intermediate/methods.go
@@ -1,4 +1,4 @@
-package main
+package intermediate
 
 import "fmt"
 
@@ -12,6 +12,24 @@ func (r Rectangle) Area() float64 {
 	return r.length * r.width
 }
 
+// Method with pointer receiver
+func (r *Rectangle) Scale(factor float64) {
+	r.length *= factor
+	r.width *= factor
+}
+
+// Method on other types
+type MyInt int
+
+func (m MyInt) IsPositive() bool {
+	return m > 0
+}
+
+// Method with embeddings
+type Shape struct {
+	Rectangle
+}
+
 func main() {
 	r := Rectangle{
 		length: 10,
@@ -19,4 +37,19 @@ func main() {
 	}
 	area := r.Area()
 	fmt.Println("Area of the rectangle:", area)
+
+	r.Scale(3)
+	area = r.Area()
+	fmt.Println("Area of the scaled rectangle:", area)
+
+	var num1 MyInt = -5
+	num2 := MyInt(9)
+
+	fmt.Println(num1.IsPositive())
+	fmt.Println(num2.IsPositive())
+
+	// embedded methods
+	s := Shape{Rectangle: Rectangle{length: 10, width: 9}}
+	fmt.Println(s.Area()) // indirect access
+	fmt.Println(s.Rectangle.Area())
 }
