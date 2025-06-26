2afe0d4d989e31e65dcaf6c75b905372decaff95
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-06-25 22:54:01 -0400

    capturing groups in a string

--- intermediate/regular_expressions.go
+++ intermediate/regular_expressions.go
@@ -22,4 +22,19 @@ func main() {
 	fmt.Println("Email1:", re.MatchString(email1))
 	fmt.Println("Email2:", re.MatchString(email2))
 	fmt.Println("Email3:", re.MatchString(email3))
+
+	// Capturing Groups
+	// Compile regex to capture data components
+	re = regexp.MustCompile(`(\d{4})-(\d{2})-(\d{2})`)
+
+	// Test string
+	date := "2024-07-30"
+
+	// Find all submatches
+	submatches := re.FindStringSubmatch(date)
+	fmt.Println(submatches)
+	fmt.Println(submatches[0])
+	fmt.Println(submatches[1])
+	fmt.Println(submatches[2])
+	fmt.Println(submatches[3])
 }
