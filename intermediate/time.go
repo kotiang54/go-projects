788acf71fc91fe387b4e2b1676338193400ef7b3
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-06-27 08:59:18 -0400

    date and time - formating and performing operations

--- intermediate/time.go
+++ intermediate/time.go
@@ -1,4 +1,4 @@
-package main
+package intermediate
 
 import (
 	"fmt"
@@ -33,4 +33,29 @@ func main() {
 
 	weekdayLater := time.Now().Add(time.Hour * 24).Weekday() // put all together
 	fmt.Println("Weekday later:", weekdayLater)
+
+	loc, _ := time.LoadLocation("Africa/Nairobi")
+	t = time.Date(2025, time.June, 27, 8, 05, 00, 00, time.UTC)
+
+	// Convert this to a specific time zone
+	tLocal := t.In(loc)
+
+	// Perform rounding
+	roundedTime := t.Round(time.Hour)
+	// roundedTimeLocal := roundedTime.In(loc)
+	roundedTimeLocal := tLocal.Round(time.Hour)
+
+	fmt.Println("Original time (UTC):", t)
+	fmt.Println("Original time (Local):", tLocal)
+	fmt.Println("Rounded time (UTC):", roundedTime)
+	fmt.Println("Rounde time (Local):", roundedTimeLocal)
+
+	// Caluclate durations
+	t1 := time.Date(2025, time.June, 27, 8, 50, 00, 00, time.UTC)
+	t2 := time.Date(2025, time.June, 27, 17, 50, 00, 00, time.UTC)
+	duration := t2.Sub(t1)
+	fmt.Println("Duration:", duration)
+
+	// Compare times
+	fmt.Println("t2 is after t1?", t2.After(t1))
 }
