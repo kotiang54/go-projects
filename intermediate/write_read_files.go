d9f9bc81806b444faee5f259dd81279563fbf23f
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-06 12:28:43 -0400

    write files using Write() and WriteString() functions

--- intermediate/write_read_files.go
+++ intermediate/write_read_files.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	file, err := os.Create("output.txt")
+	file, err := os.Create("read_write_files/output.txt")
 	if err != nil {
 		fmt.Println("Error creating file.", file)
 		return
@@ -18,6 +18,22 @@ func main() {
 	_, err = file.Write(data)
 	if err != nil {
 		fmt.Println("Error writing to file.", err)
+		return
 	}
 	fmt.Println("Data has been written to file successfully!")
+
+	// write to file using WriteString()
+	file, err = os.Create("read_write_files/writeString.txt")
+	if err != nil {
+		fmt.Println("Error creating file.", file)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("Hello Go!\n")
+	if err != nil {
+		fmt.Println("Error writing to file.", err)
+		return
+	}
+	fmt.Println("Writing to writeString.txt complete!")
 }
