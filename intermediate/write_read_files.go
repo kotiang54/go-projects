71d4a49977928089c7e57efc22e33c1b07da3fc3
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-06 13:11:01 -0400

    reading files using Read() and bufio NewScanner()

--- intermediate/write_read_files.go
+++ intermediate/write_read_files.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
+	// Write() and WriteString() functions
+	fmt.Println("=== Writing files ===")
+
 	file, err := os.Create("read_write_files/output.txt")
 	if err != nil {
 		fmt.Println("Error creating file.", file)
@@ -13,8 +17,8 @@ func main() {
 	}
 	defer file.Close()
 
-	// write dat to file
-	data := []byte("Hello World!\n")
+	// write data to file
+	data := []byte("Hello World!\n\n\nBye World.")
 	_, err = file.Write(data)
 	if err != nil {
 		fmt.Println("Error writing to file.", err)
@@ -36,4 +40,43 @@ func main() {
 		return
 	}
 	fmt.Println("Writing to writeString.txt complete!")
+
+	fmt.Println()
+	fmt.Println("=== Reading files ===")
+
+	// Read files from the write functions above
+	inFile, err := os.Open("read_write_files/output.txt")
+	if err != nil {
+		fmt.Println("Error opening file!", err)
+		return
+	}
+	defer func() {
+		fmt.Println("Closing open file")
+		inFile.Close()
+	}()
+
+	fmt.Println("File opened successfully!")
+
+	// Read the contents of the opened file
+	data = make([]byte, 1024) // buffer to read data into
+	_, err = inFile.Read(data)
+	if err != nil {
+		fmt.Println("Error reading data from file.", err)
+		return
+	}
+	fmt.Println("File content:", string(data))
+
+	// make use of buffio package
+	scanner := bufio.NewScanner(inFile)
+
+	// Read line-by-line
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println("Line:", line)
+	}
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
 }
