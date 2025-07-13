90a28fb975d7f127801b718e858878beaf1f34e4
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-13 00:22:12 -0400

    handling file system interactions in Go

--- intermediate/file_paths.go
+++ intermediate/file_paths.go
@@ -1,13 +1,14 @@
-package main
+package intermediate
 
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	// relativePath := "./data/file.txt"
-	// absolutePath := "/home/user/docs/file.txt"
+	relativePath := "./data/file.txt"
+	absolutePath := "/home/user/docs/file.txt"
 
 	// Join paths using filepath.Join()
 	joinedPath := filepath.Join("home", "Documents", "downloads", "file.zip")
@@ -22,4 +23,32 @@ func main() {
 	fmt.Println("File:", file)
 	fmt.Println("Dir:", dir)
 	fmt.Println(filepath.Base("home/user/docs/"))
+
+	// Check if existing path is relative or absolute
+	fmt.Println("Is relativePath variable absolute? ", filepath.IsAbs(relativePath))
+	fmt.Println("Is absolutePath variable absolute? ", filepath.IsAbs(absolutePath))
+
+	// Extract file extensions
+	fmt.Println(filepath.Ext(file))
+	fmt.Println(strings.TrimSuffix(file, filepath.Ext(file)))
+
+	// Convert absolute path to relative
+	rel, err := filepath.Rel("a/b", "a/b/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+
+	rel, err = filepath.Rel("a/c", "a/b/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+
+	// Convert relative path to absolute path
+	absPath, err := filepath.Abs(relativePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Absolute Path:", absPath)
 }
