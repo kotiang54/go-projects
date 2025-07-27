3aee8ce9161fa9cf28fd2692f30d729a66820697
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-26 22:05:33 -0400

    decoding xml data to Go struct

--- intermediate/xml/xml.go
+++ intermediate/xml/xml.go
@@ -26,5 +26,31 @@ func main() {
 		fmt.Println("Error marshalling XML:", err)
 		return
 	}
-	fmt.Println("XML data - person:", string(xlmData))
+	fmt.Println(string(xlmData))
+	fmt.Println("")
+	fmt.Println("XML printing with indent:")
+
+	// Using MarshalIndent for pretty printing
+	xlmData, err = xml.MarshalIndent(person, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling XML:", err)
+		return
+	}
+	fmt.Println(string(xlmData))
+
+	// XML Unmarshalling example
+	xmlRawData := `<person><name>Bob</name><age>35</age><city>Chicago</city><email>bob@example.com</email></person>`
+
+	var personData Person
+	err = xml.Unmarshal([]byte(xmlRawData), &personData)
+	if err != nil {
+		fmt.Println("Error unmarshalling XML:", err)
+		return
+	}
+	fmt.Println("Unmarshalled XML data - person:", personData)
+	fmt.Println("Name:", personData.Name)
+	fmt.Println("Age:", personData.Age)
+	fmt.Println("City:", personData.City)
+	fmt.Println("Email:", personData.Email)
+	fmt.Println("")
 }
