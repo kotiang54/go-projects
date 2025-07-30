e1d0287908b091d14f24753c333f14c32984f1e2
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-30 16:47:42 -0400

    add more examples of goroutines implementation

--- advanced/goroutines/goroutines.go
+++ advanced/goroutines/goroutines.go
@@ -11,6 +11,12 @@ import (
 func main() {
 	go sayHello()
 	// Wait for the goroutine to finish
+	fmt.Println("After sayHello()")
+
+	go printNumbers()
+	go printLetters()
+
+	// Wait for a while to let goroutines finish
 	time.Sleep(2 * time.Second)
 	fmt.Println("Main function finished.")
 }
@@ -18,4 +24,18 @@ func main() {
 func sayHello() {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Hello from Goroutine!")
+}
+
+func printNumbers() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Number:", i, time.Now())
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func printLetters() {
+	for ch := 'a'; ch < 'e'; ch++ {
+		fmt.Println(string(ch), time.Now())
+		time.Sleep(200 * time.Millisecond)
+	}
 }
