c6064e56266379eaa00ce376e1489952c174b97a
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-07-30 21:28:24 -0400

    error handling using goroutines

--- advanced/goroutines/goroutines.go
+++ advanced/goroutines/goroutines.go
@@ -9,15 +9,33 @@ import (
 // They allow concurrent execution of functions.
 
 func main() {
+	var err error
+
 	go sayHello()
 	// Wait for the goroutine to finish
 	fmt.Println("After sayHello()")
 
+	// Handle error from doSomething function
+	// Note: In a real-world scenario, you would use channels or sync.WaitGroup to
+	// synchronize goroutines and handle errors properly.
+	// Here, we are simulating an error for demonstration purposes.
+	go func() {
+		err = doSomething()
+	}()
+
+	// err = go doSomething() This is not acceptable syntax in Go.
 	go printNumbers()
 	go printLetters()
 
 	// Wait for a while to let goroutines finish
 	time.Sleep(2 * time.Second)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	} else {
+		fmt.Println("doSomething() completed successfully.")
+	}
 	fmt.Println("Main function finished.")
 }
 
@@ -38,4 +56,11 @@ func printLetters() {
 		fmt.Println(string(ch), time.Now())
 		time.Sleep(200 * time.Millisecond)
 	}
+}
+
+// Error handling
+func doSomething() error {
+	// Simulate an error
+	time.Sleep(3 * time.Second)
+	return fmt.Errorf("an error occurred while doing something")
 }
