0acf059ad69ad1a5c0d0b90f19a75393793a8f4b
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-08-29 15:50:27 -0400

    synchronize multiple goroutines using channels

--- advanced/channels/channel_synchronization.go
+++ advanced/channels/channel_synchronization.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -18,4 +19,23 @@ func main() {
 	value := <-ch // Blocking until a value is sent
 	fmt.Println("Received value:", value)
 
+	// **** Synchronizing multiple goroutines using channels ****
+	// This example launches multiple goroutines that perform work and signal completion via a channel.
+	numGoroutines := 3
+	done := make(chan int, 3)
+
+	for i := 0; i < numGoroutines; i++ {
+		go func(id int) {
+			fmt.Printf("Goroutine %d working...\n", id)
+			time.Sleep(time.Second)
+			done <- id
+		}(i)
+	}
+
+	for i := 0; i < numGoroutines; i++ {
+		<-done // Wait for each Goroutine to finish
+	}
+
+	// All goroutines have completed
+	fmt.Println("All goroutines completed")
 }
