eb3c90cab8c44be7c46182906eb0116ef8c4fc78
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-08-29 16:10:52 -0400

    synchronize data exhange between goroutines

--- advanced/channels/channel_synchronization.go
+++ advanced/channels/channel_synchronization.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -9,6 +10,7 @@ func main() {
 
 	ch := make(chan int)
 
+	// Producer goroutine sending data to dataCh
 	go func() {
 		fmt.Println("Sending ... ")
 		ch <- 9 // Blocking until the value is received
@@ -19,7 +21,7 @@ func main() {
 	value := <-ch // Blocking until a value is sent
 	fmt.Println("Received value:", value)
 
-	// **** Synchronizing multiple goroutines using channels ****
+	// ***** Synchronizing multiple goroutines using channels *****
 	// This example launches multiple goroutines that perform work and signal completion via a channel.
 	numGoroutines := 3
 	done := make(chan int, 3)
@@ -38,4 +40,21 @@ func main() {
 
 	// All goroutines have completed
 	fmt.Println("All goroutines completed")
+
+	// ***** Synchronize Data Exchange Between Goroutines *****
+	dataCh := make(chan string)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			dataCh <- "Hello " + strconv.Itoa(i)
+			time.Sleep(time.Millisecond * 100)
+		}
+		close(dataCh) // Close the channel after sending all data
+	}()
+
+	for value := range dataCh {
+		// Continuously receive data simulating a blocking operation
+		// Loops until the channel is closed
+		fmt.Println("Received:", value, ":", time.Now())
+	}
 }
