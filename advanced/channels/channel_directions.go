83bb652bdc8d2ff1964d86a40d94c5eeed65082b
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-08-31 22:57:46 -0400

    channel directions in Go programming

---
+++ advanced/channels/channel_directions.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ch := make(chan int)
+
+	// producer only channel
+	producer(ch)
+
+	// consumer only channel
+	consumer(ch)
+}
+
+// producer sends data to the channel
+func producer(ch chan<- int) {
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- i
+			fmt.Printf("Sending %v\n", i)
+		}
+		close(ch)
+	}()
+}
+
+// consumer receives data from the channel
+func consumer(ch <-chan int) {
+	for i := range ch {
+		fmt.Printf("Received %v\n", i)
+	}
+}
--- advanced/channels/channel_synchronization.go
+++ advanced/channels/channel_synchronization.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func main() {
+func synchronization() {
 
 	ch := make(chan int)
 
