eba9eefbbae7d9d8416e33f0bb16d36372fc02e3
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-09-01 22:35:39 -0400

    using select statement for multiplexing

--- advanced/channels/multiplexing.go
+++ advanced/channels/multiplexing.go
@@ -7,6 +7,32 @@ import (
 
 func main() {
 
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second)
+		ch1 <- "Message from ch1"
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		ch2 <- "Message from ch2"
+	}()
+
+	for range 2 {
+		select {
+		case msg := <-ch1:
+			fmt.Println("Received from ch1:", msg)
+		case msg := <-ch2:
+			fmt.Println("Received from ch2:", msg)
+			// default:
+			// 	fmt.Println("No messages received")
+		}
+	}
+
+	fmt.Println("End of program")
+
 	ch := make(chan int)
 
 	go func() {
