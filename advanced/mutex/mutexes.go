c5e190c8899b391b03719988c0df79322e4274f1
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-09-15 23:16:16 -0400

    using mutexes on non struct types

--- advanced/mutex/mutexes.go
+++ advanced/mutex/mutexes.go
@@ -23,12 +23,16 @@ func (c *Counter) getCount() int {
 }
 
 func main() {
+	// *** Demonstrating Mutex for Safe Concurrent Access ***
+	// This example shows how to use a mutex to safely increment a counter from multiple goroutines.
+
+	// Create a WaitGroup to wait for all goroutines to finish
 	var wg sync.WaitGroup
 	counter := &Counter{}
 
 	numGoroutines := 10
 
-	wg.Add(numGoroutines)
+	// wg.Add(numGoroutines)
 
 	for range numGoroutines {
 		wg.Add(1)
@@ -42,4 +46,31 @@ func main() {
 
 	wg.Wait()
 	fmt.Printf("Final Count: %d\n", counter.getCount())
+
+	// *** Demonstration without a struct (not recommended) ***
+	// This example shows how to use a mutex to protect a simple integer variable.
+
+	var (
+		count int
+		mu    sync.Mutex
+	)
+
+	numGoroutines = 10
+	wg.Add(numGoroutines)
+
+	increment := func() {
+		defer wg.Done()
+		for range 1000 {
+			mu.Lock()
+			count++
+			mu.Unlock()
+		}
+	}
+
+	for range numGoroutines {
+		go increment()
+	}
+
+	wg.Wait()
+	fmt.Printf("Final Count without struct: %d\n", count)
 }
