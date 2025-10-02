7cc4b92ea3c032c4cd5896a15aec05f06a547b05
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-10-02 11:58:47 -0400

    implement pipe commands in Go

--- advanced/os_processes/os_processes.go
+++ advanced/os_processes/os_processes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
 	"time"
@@ -9,8 +10,18 @@ import (
 
 func main() {
 
-	cmd := exec.Command("echo", "Hello World!")
+	// **** Running a simple command and capturing its output ****
+	cmd := exec.Command("printenv", "SHELL")
 	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error executing command:", err)
+		return
+	}
+	fmt.Println("Output:", string(output))
+
+	// Another example with echo
+	cmd = exec.Command("echo", "Hello World!")
+	output, err = cmd.Output()
 	if err != nil {
 		fmt.Println("Error executing command:", err)
 		return
@@ -27,6 +38,8 @@ func main() {
 	}
 	fmt.Println("Grep Output:\n", string(output))
 
+	// **** Starting a long-running process and managing it ****
+	// Here we use "sleep" command to simulate a long-running process
 	cmd = exec.Command("sleep", "3")
 	err = cmd.Start()
 	if err != nil {
@@ -39,14 +52,6 @@ func main() {
 	// Process ID
 	fmt.Println("Process ID:", cmd.Process.Pid)
 
-	// Killing the process after 2 seconds of sleep
-	err = cmd.Process.Kill()
-	if err != nil {
-		fmt.Println("Error killing command:", err)
-		return
-	}
-	fmt.Println("Process killed")
-
 	// Waiting: comment this out to see the difference
 	err = cmd.Wait()
 	if err != nil {
@@ -54,4 +59,29 @@ func main() {
 		return
 	}
 	fmt.Println("Command finished successfully")
+
+	// Killing the process after 2 seconds of sleep
+	// Comment the Wait() above to see the kill in action
+	// err = cmd.Process.Kill()
+	// if err != nil {
+	// 	fmt.Println("Error killing command:", err)
+	// 	return
+	// }
+	// fmt.Println("Process killed")
+
+	pr, pw := io.Pipe()
+	cmd = exec.Command("grep", "foo")
+	cmd.Stdin = pr
+
+	go func() {
+		defer pw.Close()
+		pw.Write([]byte("food is good\nbar\nbaz\nfoo bar izo"))
+	}()
+
+	output, err = cmd.Output()
+	if err != nil {
+		fmt.Println("Error executing command:", err)
+		return
+	}
+	fmt.Println("Grep Output with Pipe:\n", string(output))
 }
