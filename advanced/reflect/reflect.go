35c643edd1e4f31668acaf5f4afc99407d21a583
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-10-06 18:05:13 -0400

    using structs with reflection

--- advanced/reflect/reflect.go
+++ advanced/reflect/reflect.go
@@ -14,6 +14,12 @@ import (
 // 		implementing generic functions, and
 // 		building frameworks that require type introspection.
 
+// Structs with reflect package
+type Person struct {
+	name string
+	age  int
+}
+
 func main() {
 
 	var x float64 = 3.54
@@ -34,4 +40,39 @@ func main() {
 	y := 10
 	v1 := reflect.ValueOf(&y).Elem()
 	fmt.Println("Value v1:", v1)
+	fmt.Println("Original value:", v1.Int())
+
+	v1.SetInt(20)
+	fmt.Println("New value:", v1.Int())
+	fmt.Println("Original variable:", y)
+
+	var itf interface{} = "Hello"
+	v2 := reflect.ValueOf(itf)
+
+	fmt.Println("")
+	fmt.Println("Type v2:", v2.Type())
+	if v2.Kind() == reflect.String {
+		fmt.Println("String value:", v2.String())
+	}
+
+	// Struct with reflection
+	p := Person{
+		name: "Jane Doe",
+		age:  30,
+	}
+
+	v = reflect.ValueOf(p)
+	fmt.Println("")
+
+	for i := range v.NumField() {
+		fmt.Printf("Field %d: %v\n", i, v.Field(i))
+	}
+
+	v1 = reflect.ValueOf(&p).Elem()
+	nameField := v1.FieldByName("name")
+	if nameField.CanSet() {
+		nameField.SetString("John Doe")
+	} else {
+		fmt.Println("Cannot set name field")
+	}
 }
