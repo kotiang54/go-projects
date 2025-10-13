bbffd335e05634c0420fa9f055c4bd43bc2cbb90
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-10-13 12:25:25 -0400

    create a tls custom server for http2

--- API-Projects/REST-API/server.go
+++ API-Projects/REST-API/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -26,10 +30,41 @@ func main() {
 	// Define the port to listen on
 	port := 3000
 
-	// Start the server
-	fmt.Println("Server is running on port", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// Modify the code to handle TLS/SSL for secure communication (HTTPS)
+	// You will need to generate a self-signed certificate for testing purposes
+	// In a production environment, use a valid SSL certificate from a trusted CA
+
+	// For HTTPS, use ListenAndServeTLS method instead
+	// Make sure to generate cert.pem and key.pem files using OpenSSL
+	// You can use the following command to generate a self-signed certificate:
+	// 	- openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365
+
+	// Load the TLS cert and key
+	cert := "cert.pem"
+	key := "key.pem"
+
+	// configure the TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// Create a custom server
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		TLSConfig: tlsConfig,
+		Handler:   nil, // Use default mux
+	}
+
+	// Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
+
+	fmt.Println("Server is running on port:", port)
+	// Start the server with HTTP 1.1 - Server without TLS
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	// Start the server with HTTP 2.0 - Server with TLS
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		log.Fatalln("Error starting server:", err)
 	}
 }
