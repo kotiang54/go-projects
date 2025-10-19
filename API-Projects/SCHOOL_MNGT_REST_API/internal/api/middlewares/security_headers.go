3034525e91568e28e35fdbeda6465e042d920feb
Stephen Kotiang <22131605+kotiang54@users.noreply.github.com>
2025-10-19 10:29:14 -0400

    implement CORS middlewares

--- API-Projects/SCHOOL_MNGT_REST_API/internal/api/middlewares/security_headers.go
+++ API-Projects/SCHOOL_MNGT_REST_API/internal/api/middlewares/security_headers.go
@@ -1,6 +1,24 @@
 package middlewares
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+// Allow multiple origins
+var allowedOrigins = []string{
+	"https://localhost:3000",
+	"https://my-origin-url.com",
+}
+
+func isOriginAllowed(origin string) bool {
+	for _, allowedOrigin := range allowedOrigins {
+		if origin == allowedOrigin {
+			return true
+		}
+	}
+	return false
+}
 
 func SecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +40,35 @@ func SecurityHeaders(next http.Handler) http.Handler {
 
 		// Legacy (optional; modern Chrome/Edge ignore it)
 		// h.Set("X-XSS-Protection", "1; mode=block")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// CORS - Cross-Origin resource sharing
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		fmt.Println(origin)
+
+		if !isOriginAllowed(origin) {
+			http.Error(w, "Not allowed by CORS", http.StatusForbidden)
+			return
+		}
+
+		h := w.Header()
+		h.Set("Access-Control-Allow-Origin", origin)
+		h.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		h.Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		h.Set("Access-Control-Allow-Credentials", "true")
+		h.Set("Access-Control-Expose-Headers", "Authorization")
+		h.Set("Access-Control-Max-Age", "3600")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
